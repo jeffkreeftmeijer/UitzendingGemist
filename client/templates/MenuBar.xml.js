1cea4d1e20c6f716328352a0fa3ea4a23a06bf81
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2015-11-08 21:01:01 +0100

    Put the new episodes view first in the menu bar

--- client/templates/MenuBar.xml.js
+++ client/templates/MenuBar.xml.js
@@ -2,11 +2,11 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
 <document>
   <menuBarTemplate>
     <menuBar>
-      <menuItem view="popular">
-        <title>Populair</title>
-      </menuItem>
       <menuItem view="recent">
         <title>Nieuw</title>
+      </menuItem>
+      <menuItem view="popular">
+        <title>Meest bekeken</title>
       </menuItem>
     </menuBar>
   </menuBarTemplate>
